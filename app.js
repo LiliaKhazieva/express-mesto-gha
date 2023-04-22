af22b776d73168b571bc5df632b880237d7c7b6d
Lilia Khazieva <lilia002777@mail.ru>
2023-04-22 21:08:06 +0300

    change startup port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect('mongodb://localhost:27017/mestodb')
